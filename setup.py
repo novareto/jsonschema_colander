e69dd8822a3d1487de4f8cd7bfe04a6c394f0b5d
Souheil Chelfouh <trollfot@gmail.com>
2026-01-16 12:39:28 +0100

    migrate to pyproject

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
-from setuptools import setup
+import setuptools
 
-
-setup(
-    name='jsonschema_colander',
-    install_requires=[
-        'colander>=1.8',
-        'jsonschema',
-        'unflatten'
-    ],
-    extras_require={
-        'test': [
-            'pytest>=3',
-            'PyHamcrest'
-        ]
-    }
-)
+setuptools.setup()
